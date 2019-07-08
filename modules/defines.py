67e657cda1ac76b81c4d44f9edbf1a9508a043a8
Philmore971 <44825916+Philmore971@users.noreply.github.com>
2019-07-07 23:25:03 -0400

    Dernier TOC 80200
    TOC par défaut

--- modules/defines.py
+++ modules/defines.py
@@ -12,7 +12,7 @@ LCURSE_ADDON_TOCS_CACHE = LCURSE_FOLDER + "/tocs.json"
 LCURSE_MAXTHREADS_KEY = "Preferences/maxthreads"
 LCURSE_MAXTHREADS_DEFAULT = 50
 LCURSE_DBVERSION = 1
-TOC = "80100"
+TOC = "80200"
 print("Folders",WOW_FOLDER_KEY,WOW_FOLDER_DEFAULT)
 # with open(toc, encoding="utf8", errors='replace') as f:
 #     line = f.readline()
