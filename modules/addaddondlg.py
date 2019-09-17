1b873ace219d3dc0933e6054ad8c3593fe1bdea2
Philmore971 <44825916+Philmore971@users.noreply.github.com>
2019-09-17 14:21:19 -0400

    Mise à jour du 17/09/2019

--- modules/addaddondlg.py
+++ modules/addaddondlg.py
@@ -5,7 +5,7 @@ class AddAddonDlg(Qt.QDialog):
     def __init__(self, parent, availableAddons):
         super(AddAddonDlg, self).__init__(parent)
         box = Qt.QVBoxLayout(self)
-        box.addWidget(Qt.QLabel(self.tr("Type name or url of the addon you want to add:"), self))
+        box.addWidget(Qt.QLabel(self.tr("Donnez le nom ou l'URL de l'addon que vous souhaitez ajouter :"), self))
         self.input = Qt.QLineEdit(self)
         box.addWidget(self.input)
         btnBox = Qt.QDialogButtonBox(Qt.QDialogButtonBox.Ok | Qt.QDialogButtonBox.Cancel)
@@ -19,9 +19,9 @@ class AddAddonDlg(Qt.QDialog):
             self.completer.setCaseSensitivity(Qt.Qt.CaseInsensitive)
             self.input.setCompleter(self.completer)
         else:
-            Qt.QMessageBox.information(self, self.tr("No addon catalog data"), self.tr(
-                "You haven't updated the available addons catalog, "
-                "so you need to insert a URL for the addon you want to add."))
+            Qt.QMessageBox.information(self, self.tr("Catalogue des addons absent"), self.tr(
+                "Vous n'avez pas mis à jour le catalogue des addons disponibles, "
+                "donnez l'URL de l'addon que vous souhaitez ajouter."))
 
     def getText(self):
         return self.input.text()
